62085ae1b8f1890eb37a5c172e32acb7a7da042e
Mario Rizki <mariorizki07@gmail.com>
2020-04-16 01:09:43 +0700

    add hero section

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `learn gatsby`,
+    title: `Learn Gatsby`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
