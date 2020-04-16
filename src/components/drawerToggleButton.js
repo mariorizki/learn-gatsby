f4703f1795de0cfeb5fdf86f181a3a2d3574ce3d
Mario Rizki <mariorizki07@gmail.com>
2020-04-17 00:40:24 +0700

    add drawer toggle button on navigation

--- src/components/drawerToggleButton.js
+++ src/components/drawerToggleButton.js
@@ -1,9 +1,13 @@
 import React from "react"
 
+import "./drawerToggleButton.css"
+
 const drawerToggleButton = props => (
-  <button>
+  <button className="toggle-button">
     <div className="toggle-button-line"></div>
     <div className="toggle-button-line"></div>
     <div className="toggle-button-line"></div>
   </button>
 )
+
+export default drawerToggleButton
--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,27 @@
 import React from "react"
 import Link from "gatsby-link"
+import DrawerToggleButton from "./drawerToggleButton"
 
 import "./header.css"
 
 const header = ({ siteTitle }) => (
   <div className="Header">
     <div className="HeaderGroup">
-      <Link to="/">
-        <img src={require("../images/gatsby-icon.png")} alt="logo" width="30" />
+      <div>
+        <DrawerToggleButton />
+      </div>
+
+      <img src={require("../images/gatsby-icon.png")} alt="logo" width="30" />
+
+      <Link to="/" className="header-menu">
+        Menu 1
       </Link>
-      <Link to="/">Menu 1</Link>
-      <Link to="/">Menu 2</Link>
-      <Link to="/">Menu 3</Link>
+      <Link to="/" className="header-menu">
+        Menu 2
+      </Link>
+      <Link to="/" className="header-menu">
+        Menu 3
+      </Link>
       <img
         className="LightIcon"
         src={require("../images/sun.svg")}
